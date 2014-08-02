75bf74a586dfdf81325bffeccce8d3637101d684
BillyWolfington <willwolf@umich.edu>
2014-08-02 12:22:46 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Billy Wolfington on 8/2/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Gameplay : CCNode
+
+@end
